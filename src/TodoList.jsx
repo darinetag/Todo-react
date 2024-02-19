f48ca8bdd344cc87d333bef4a90745be4019613a
Darine <tagdarinefatma@gmail.com>
2024-02-19 17:56:46 +0100

    todo list completed

---
+++ src/TodoList.jsx
@@ -0,0 +1,32 @@
+import { TodoItem } from "./TodoItem"
+import PropTypes from "prop-types";
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo => {
+        return (
+          <TodoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        )
+      })}
+    </ul>
+  )
+}
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        completed: PropTypes.bool.isRequired,
+      })
+    ).isRequired,
+    toggleTodo: PropTypes.func.isRequired,
+    deleteTodo: PropTypes.func.isRequired,
+  };
